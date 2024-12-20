6fca61a6d252f49459bedace5fb74bdb54f6cb55
Patrick <Pwkjr1@gmail.com>
2024-12-20 12:09:12 -0800

    Modularized folder structure to include widget categories. Architected expenses to expense -> expenses_list -> expense_item for modularity and reusability.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -29,11 +30,13 @@ class _Expenses extends State<Expenses> {
 
   @override
   Widget build(context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('Chart'),
-          Text('Expenses'),
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -14,7 +15,9 @@ class ExpensesList extends StatelessWidget {
     //Review: ListView.builder() allows items to be loaded dyanamically.
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => Text(expenses[index].title),
+      itemBuilder: (context, index) => ExpenseItem(
+        expense: expenses[index],
+      ),
     );
   }
 }
