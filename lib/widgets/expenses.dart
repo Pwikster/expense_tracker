724049e4005af60d13bc46d88aacfaa8c61f8cc6
Patrick <Pwkjr1@gmail.com>
2024-12-27 11:39:46 -0800

    SnackBar tweaks

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -50,6 +50,7 @@ class _Expenses extends State<Expenses> {
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 3),
