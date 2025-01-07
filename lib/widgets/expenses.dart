9ba16ec4faf62e908401bf8475054b11ab46101b
Patrick <Pwkjr1@gmail.com>
2025-01-07 14:46:24 -0800

    Implemented responsive layout for landscape mode.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -70,6 +70,8 @@ class _Expenses extends State<Expenses> {
 
   @override
   Widget build(context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No Expenses Yet!'),
     );
@@ -91,12 +93,21 @@ class _Expenses extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
